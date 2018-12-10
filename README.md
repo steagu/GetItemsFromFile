c09dda09f49d2c05a694a676206fed0e472a33fe
Samuel Teague <steague@bgsu.edu>
2018-12-10 11:03:07 -0500

    Updated README and wrote function

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # GetItemsFromFile
 Pulls items from a file and stores them as a vector of strings; each string is one line of the file
+
+Sample API usage:
+
+varName = getItemsfromFile(fileName) 
+where varName is a vector of strings.
+
+This function will throw a file_not_found (inherits from std::logic_error) exception if file is not found
+Can also throw std::bad_alloc from the vector if the file being worked with is absurdly large
---
+++ getItemsFromFile.hpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <string>
+#include <exception>
+
+class file_not_found : public std::logic_error
+{
+public:
+    file_not_found(std::string s) : std::logic_error(s) {}
+}
+
+std::vector<std::string> getItemsFromFile(std::string fileName)
+{
+    /*
+        @param: takes a string fileName, the file to from.
+        @return: a vector of strings, each string holding the name and description of a 
+            single Star Trek episode
+    */
+    std::vector<std::string> toFill;
+    std::ifstream file;
+
+    file.open(fileName);
+
+    if (!file.is_open())
+        throw file_not_found("Unable to open file " + fileName);
+
+    std::string episodeData;
+    while(!file.eof())
+    {
+
+        std::getline(file, episodeData);
+
+        toFill.push_back(episodeData);
+
+        episodeData = "";
+    }
+    file.close();
+    
+    return toFill;
+}
